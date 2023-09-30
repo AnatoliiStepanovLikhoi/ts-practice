1eb27f2e6e1e4294448f5d5e0e60418b9a787f63
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-09-30 15:30:26 +0300

    add: lesson3 files

---
+++ lesson3/01-arrays/01-foreach.ts
@@ -0,0 +1,24 @@
+const myArray: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// forEach - обхід всіх елементів масиву та запуск певної функції для значення кожного елемента.
+const myCallback = (value: number) => {
+  const message = value % 2 === 0 ? "Парне число" : "Непарне число";
+  console.log(value + " - " + message);
+};
+myArray.forEach(myCallback);
+
+// збільшення кожного елемента масиву на 1
+myArray.forEach((value: number, index: number, array: number[]) => {
+  array[index] = value + 1;
+});
+console.log(myArray);
+
+// список літер які можуть повторятись
+const letters = ["a", "b", "c", "a", "c", "a"];
+// в цю змінну ми запишимо унікальні значення літер і кількість повторень
+const repeats = {};
+
+letters.forEach((letter) =>
+  repeats[letter] ? (repeats[letter] += 1) : (repeats[letter] = 1)
+);
+console.log(repeats);
