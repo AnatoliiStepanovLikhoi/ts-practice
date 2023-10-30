265bc4648bc6aecf7c7e39fd512d59ba1b5e41dc
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-10-30 15:15:09 +0200

    add: functions type annotation

--- lesson1/02-functions/01-functions.ts
+++ lesson1/02-functions/01-functions.ts
@@ -23,12 +23,54 @@ const logNumber = (i: number): void => {
   console.log(i);
 };
 
+const add3 = (a: number, b: number): number => {
+  return a + b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+
+  return message;
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
+
 //*When use annotations
 
 // 1.When function return 'any'
 const json = '{"x": 10, "y": 20}';
 const cordinate: { x: number; y: number } = JSON.parse(json);
-console.log(cordinate);
+// console.log(cordinate);
 
 // 2. When we declare a variable on one line and initilize later
 
