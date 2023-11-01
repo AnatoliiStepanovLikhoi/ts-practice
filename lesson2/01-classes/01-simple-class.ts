bea1f575d3744e2a0e3d693b310a2d4d9e7913e8
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-11-01 23:33:06 +0200

    add: class method modifiers

--- lesson2/01-classes/01-simple-class.ts
+++ lesson2/01-classes/01-simple-class.ts
@@ -4,18 +4,40 @@ class User {
 
   // метод
   print(): void {
-    console.log(this.firstName + " " + this.lastName);
+    console.log(this.firstName + ' ' + this.lastName);
   }
 }
 
 const user1 = new User(); // створюємо екземпляр класу User викликаючи конструктор та ініціалізуємо змінну user1
-user1.firstName = "Ivan"; // присвоєння значення властивості firstName на екземплярі user1
-user1.lastName = "Ivanov";
+user1.firstName = 'Ivan'; // присвоєння значення властивості firstName на екземплярі user1
+user1.lastName = 'Ivanov';
 
 const user2: User = new User(); // створюємо екземпляр класу User викликаючи конструктор та ініціалізуємо змінну user2
-user2.firstName = "John"; // присвоєння значення властивості firstName на екземплярі user2
-user2.lastName = "Doe";
+user2.firstName = 'John'; // присвоєння значення властивості firstName на екземплярі user2
+user2.lastName = 'Doe';
 
 user1.print(); // виклик методу print на екземплярі user1
 user2.print(); // виклик методу print на екземплярі user2
 console.log(user1);
+
+//
+class Vehicle {
+  protected honk(): void {
+    console.log('beep');
+  }
+}
+
+class Car extends Vehicle {
+  private drive(): void {
+    console.log('shhhh');
+  }
+
+  startDriving(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car();
+car.startDriving();
+// car.honk();
