66d88f210b57a822992f74b4adc7725a645ed829
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-11-07 00:26:02 +0200

    add: card implementation

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,7 +1,33 @@
-import { User } from './User';
-import { Company } from './Company';
+// import { User } from './User';
+// import { Company } from './Company';
+import * as dotenv from 'dotenv';
+import { Loader } from '@googlemaps/js-api-loader';
 
-const user = new User();
-const company = new Company();
+// const map = document.getElementById('map')!;
 
-console.log(user, company);
+// new google.maps.Map(map, {
+//   zoom: 8,
+//   center: {
+//     lat: -34.397,
+//     lng: 150.644,
+//   },
+// });
+
+dotenv.config();
+
+console.log(process.env.GOOGLE_MAPS_API_KEY);
+
+const loader = new Loader({
+  apiKey: process.env.GOOGLE_MAPS_API_KEY as string,
+  version: 'weekly',
+});
+
+loader.load().then(async () => {
+  const { Map } = (await google.maps.importLibrary(
+    'maps'
+  )) as google.maps.MapsLibrary;
+  new Map(document.getElementById('map') as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 1,
+  });
+});
